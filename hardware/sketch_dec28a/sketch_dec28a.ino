ae5eccaf82125ece2c58d2f21424435c88b7454e
dispiny <pjm1024cl@itnsa.cloud>
2021-12-29 09:53:34 +0900

    Update: hardware.ino

--- hardware/sketch_dec28a/sketch_dec28a.ino
+++ hardware/sketch_dec28a/sketch_dec28a.ino
@@ -5,17 +5,14 @@
 
 #define P1_BTN 8
 #define P2_BTN 9
-
+#define P3_BTN 10
 #define ledPin 13
-
 #define P1_MOTOR_A 2
-#define P1_MOTOR_ENA 3
-#define P1_MOTOR_B 4
-
-#define P2_MOTOR_A 5
-#define P2_MOTOR_ENA 6
-#define P2_MOTOR_B 7
-
+#define P1_MOTOR_B 3
+#define P2_MOTOR_A 4
+#define P2_MOTOR_B 5
+#define P3_MOTOR_A 6
+#define P3_MOTOR_B 7
 #define SERVO_PIN 12
 
 Servo servo;      // Servo 클래스로 servo라는 Object 생성
@@ -42,11 +39,9 @@ void setup() {
 
   pinMode(P1_BTN, INPUT);     // 2번 핀 입력모드
   pinMode(P2_BTN, INPUT);     // 3번 핀 입력모드
-  pinMode(4, OUTPUT);
-  pinMode(P1_MOTOR_A, OUTPUT);
-  pinMode(P2_MOTOR_A, OUTPUT);
-  pinMode(P2_MOTOR_B, OUTPUT);
-
+  pinMode(P3_BTN, INPUT);     // 4번 핀 입력모드
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
 
   Serial.begin(9600); // 시리얼 통신 시작
 
@@ -59,7 +54,7 @@ void setup() {
   lcd.clear();    // LCD 화면 클리어
   lcd.setCursor(3, 0);
   lcd.print("WARMING UP");    // 게임 준비 화며
-  // delay(9000);    // 9초 대기
+  delay(9000);    // 9초 대기
   lcd.clear();
 
   randomSeed(analogRead(0));
@@ -67,6 +62,8 @@ void setup() {
 
 void loop()
 {
+  digitalWrite(4,1);
+  digitalWrite(5,0);
   unsigned long currentMillis = millis();   // ㅅmills 함수사용할 때 현재 값 저장
   rate = random(2, 5);    // 영희 소리 배속 (랜덤)
 
@@ -126,16 +123,12 @@ void PLAYER_BTN_EVENT(int rate) {
     Serial.print(" angle180");
     Serial.println();
   }
-  int p1Status = digitalRead(P1_BTN);
+  
   if (digitalRead(P1_BTN) == HIGH) {
     Serial.println("p1");
-    Serial.print("p1Status: ");
-    Serial.println(p1Status);
-    digitalWrite(P1_MOTOR_A, HIGH);
-    digitalWrite(P1_MOTOR_B, LOW);
-    analogWrite(P1_MOTOR_ENA, 40);
-  }
-  if (p1Status == HIGH && speakValue == 1) {
+    analogWrite(P1_MOTOR_A, 80);
+    analogWrite(P1_MOTOR_B, 0);
+  } else if (digitalRead(P1_BTN) == HIGH && speakValue == 1) {
     Serial.println("FAILEP1");    // Serial에 출력
     delay(500);
     digitalWrite(ledPin, HIGH);   // LED 켜고 1초뒤 종료
@@ -144,16 +137,12 @@ void PLAYER_BTN_EVENT(int rate) {
     speakValue_p1_fail = 1;
     LCD_PRINT_FAILE_PLAYER(1);
   }
-  int p2Status = digitalRead(P2_BTN);
-  if (digitalRead(P2_BTN) == HIGH) {
+
+  if (digitalRead(P2_BTN)==HIGH) {
     Serial.println("p2");
-    Serial.print("p2Status: ");
-    Serial.println(p2Status);
-    digitalWrite(P2_MOTOR_A, HIGH);
-    digitalWrite(P2_MOTOR_B, LOW);
-    analogWrite(P2_MOTOR_ENA, 60);
-  }
-  if (p2Status == HIGH && speakValue == 1) {
+    analogWrite(P2_MOTOR_A, 80);
+    analogWrite(P2_MOTOR_B, 0);
+  } else if (digitalRead(P2_BTN) == HIGH && speakValue == 1) {
     Serial.println("FAILEP2");    // Serial에 출력
     delay(500);
     digitalWrite(ledPin, HIGH);   // LED 켜고 1초뒤 종료
@@ -161,6 +150,20 @@ void PLAYER_BTN_EVENT(int rate) {
     digitalWrite(ledPin, LOW);
     speakValue_p2_fail = 1;
     LCD_PRINT_FAILE_PLAYER(2);
+  }
+
+  if (digitalRead(P3_BTN)==HIGH) {
+    Serial.println("p3");
+    analogWrite(P3_MOTOR_A, 80);
+    analogWrite(P3_MOTOR_B, 0);
+  } else if (digitalRead(P3_BTN) == HIGH && speakValue == 1) {
+    Serial.println("FAILEP3");    // Serial에 출력
+    delay(500);
+    digitalWrite(ledPin, HIGH);   // LED 켜고 1초뒤 종료
+    delay(1000);
+    digitalWrite(ledPin, LOW);
+    speakValue_p3_fail = 1;
+    LCD_PRINT_FAILE_PLAYER(3);
   }
   speakValue = 0;
 }
